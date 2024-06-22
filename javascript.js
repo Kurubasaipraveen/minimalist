5cdcab872c0f69d8c5aa66b2262c4e602833db5d
Kurubasaipraveen <154404195+Kurubasaipraveen@users.noreply.github.com>
2024-06-22 15:07:45 +0530

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// Open the popup
+document.getElementById('openPopup1').addEventListener('click', function() {
+    document.getElementById('popup1').style.display = 'block';
+});
+
+// Close the popup
+document.getElementById('closePopup1').addEventListener('click', function() {
+    document.getElementById('popup1').style.display = 'none';
+});
+
+document.getElementById('openPopup2').addEventListener('click', function() {
+    document.getElementById('popup2').style.display = 'block';
+});
+
+// Close the popup
+document.getElementById('closePopup2').addEventListener('click', function() {
+    document.getElementById('popup2').style.display = 'none';
+});
